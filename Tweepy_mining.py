7f4462350d26c37691cad52916b8af8aa8fe1aff
Tuvannguyen <tuvannguyen33@gmail.com>
2017-09-23 14:07:27 -0500

    Adding filename argument to stream object

--- Tweepy_mining.py
+++ Tweepy_mining.py
@@ -10,13 +10,8 @@ from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 import csv
 
-apple_words= ['Apple', 'iPhone', 'iPad', 'Mac', 'iOS']
-
 ###DO NOT INCLUDE In SHARED FILE###
-consumer_key = 'hQSqSVXIKuydP6wwQXhN0Q61E'
-consumer_secret = 'CpFWabKVlGn27WzaQrvTspQhC0oOXPNkg8gjDNgWyjZMaH2oFT'
-access_token = '911467311328002048-NtCItPcv277CqOIbIYikIUcwW0msGGh'
-access_token_secret = 'XMl0UMBXWsZpKtWEiU0jVaW7aDwYJiDe0Vq7mJW4pPQ62'
+
 ####################################
 
 #modify Streamlistener to print out stream
@@ -25,12 +20,12 @@ class MyStreamListener(StreamListener):
 
 
     #initialize values
-    def __init__(self, start_time=time.time(), api=None, time_limit = 5):
+    def __init__(self, filename, start_time=time.time(), api=None, time_limit = 5):
         self.api = api
         self.start = start_time
         self.end = time_limit+start_time
         self.tweets = [] #stores all data to add to file
-        
+        self.file = filename
     #what happens on each iteration
     def on_status(self, status):
         while (time.time() <self.end): 
@@ -38,33 +33,33 @@ class MyStreamListener(StreamListener):
             print("Getting tweet from %s" %(status._json['user']['screen_name']))
             return True #somehow stops program from reprinting same tweet
         
-        with open('twitter.csv', 'w', newline='', encoding = 'utf-8') as csvfile:
+        with open(self.file, 'w', newline='', encoding = 'utf-8') as csvfile:
             writer = csv.writer(csvfile)
             for tweet in self.tweets:
-                writer.writerow([tweet._json['user']['screen_name'], tweet._json['text'],
-                                 tweet._json['retweet_count']])
+                #writer.writerow([tweet._json['user']['screen_name'], tweet._json['text'],
+                #                tweet._json['retweet_count']])
                 print('@%s tweeted: %s' %(tweet._json['user']['screen_name'], tweet._json['text']))
-                 
+                
     def on_error(self, status_code):
         if status_code == 420:
             #returning False in on_data disconnects the stream
             return False
 
 #Create OAuthHandler instance
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#Create stream listener
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+apple_words= ['Apple', 'iPhone', 'iPad', 'Mac', 'iOS']
+google_words = ['Google','google','gmail']
+microsoft_words = ['Microsoft', 'microsoft', 'windows', 'powerpoint']
+samsung = ['android', 'galaxy tab', 'samsung']
 
-#filters out tweets with keywords and in english
-myStream.filter(track=apple_words, languages = ['en'])
+#Create stream listeners and filters
+AppleListener = MyStreamListener(filename = 'Apple.csv', time_limit = 4)
+AppleStream = tweepy.Stream(auth = api.auth, listener=AppleListener )
+AppleStream.filter(track=apple_words, languages = ['en'])
 
 
-
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#     print(tweet.text)
