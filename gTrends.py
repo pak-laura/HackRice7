165919eccffbe103df4816a98b40732712e798a1
Laura Pak <laurapak2@gmail.com>
2017-09-23 17:29:12 -0500

    time frame gTrends 1 day

--- gTrends.py
+++ gTrends.py
@@ -7,7 +7,7 @@ from pytrends.request import TrendReq
 pytrend= TrendReq()
 
 #create payload, capture API tokens? only for time, region, related queries
-pytrend.build_payload(kw_list=['iPhone', 'iPad','Apple','iOS'],timeframe ='today 3-m')
+pytrend.build_payload(kw_list=['iPhone', 'iPad','Apple','iOS'],timeframe ='now 1-d')
 
 interest_over_time_df = pytrend.interest_over_time()
 print(interest_over_time_df.head())
