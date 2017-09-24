86c5f8a24f43f43f254447ef4cf99f61cebdc296
Laura Pak <laurapak2@gmail.com>
2017-09-23 20:54:30 -0500

    sent.py is not writing to csv file:(

--- sent.py
+++ sent.py
@@ -3,35 +3,52 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import csv
 import pandas
+import re
 
-
-fileCo = 'Microsoft.csv'
-colnames = ['user','text','retweets']
+fileCo = 'Google.csv'
+colnames = ['user','text','date']
 data = pandas.read_csv(fileCo,names=colnames)
-list0 = data.text.tolist()
-
+listText = data.text.tolist()
+listDate = data.date.tolist()
 sent_analyzer = SentimentIntensityAnalyzer()
 
 pos_count=0
 neg_count=0
 neu_count=0
-total_count=len(list0)
+total_count=0
+date0 = listDate[0][4:10]
+with open('results.csv', 'w', newline='') as csvfile:
+	wrote = csv.writer(csvfile)
 
-for tweet in list0:
-	ss = sent_analyzer.polarity_scores(tweet)
-	if ss['compound'] > 0:
-		pos_count += 1
-	elif ss['compound'] < 0:
-		neg_count += 1
-	else:
-		neu_count += 1
-	print(tweet)
-	for k in sorted(ss):
+	for tweet, date in zip(listText, listDate):
+		if date[4:10] != date0: #new date
+			wrote.writerow([date[4:10], pos_count/total_count, neg_count/total_count, neu_count/total_count])
+			pos_count=0
+			neg_count=0
+			neu_count=0
+			total_count=0
+			date0 = date[4:10]
+
+		ss = sent_analyzer.polarity_scores(tweet)
+		if ss['compound'] > 0:
+			pos_count += 1
+		elif ss['compound'] < 0:
+			neg_count += 1
+		else:
+			neu_count += 1
+		total_count += 1
+
+#	print(tweet)
+#	for k in sorted(ss):
 	#	k = compound,neg,neu,pos, ss[k] = some number
-		print('{0}: {1}, '.format(k,ss[k]),end ='')
-	print() #adds newline after scores print
-
+#		print('{0}: {1}, '.format(k,ss[k]),end ='')
+#	print() #adds newline after scores print
+'''
 print("Positive tweets: ",pos_count/total_count,"%")
 print("Negative tweets: ",neg_count/total_count,"%")
 print("Neutral tweets: ",neu_count/total_count,"%")
 print("total tweets: ",total_count)
+'''
+
+
+
